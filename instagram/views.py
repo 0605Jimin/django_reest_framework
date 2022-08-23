462dd4fcf7e28a6b5961f8655bf3526a4252f2bb
Jimin Kim <wweww1596@likelion.org>
2022-08-23 16:22:56 +0900

    JSON 직렬화

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,13 @@
+from sysconfig import is_python_build
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
+
+class PublicPostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all() # filter(is_public=True)
+    serializer_class = PostSerializer
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
