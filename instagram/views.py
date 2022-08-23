0ec9f687b4c37ad482e88b5c55e50140ed865d95
Jimin Kim <wweww1596@likelion.org>
2022-08-23 17:22:30 +0900

    APIView를 활용한 뷰 만들기

--- instagram/views.py
+++ instagram/views.py
@@ -1,13 +1,37 @@
-from sysconfig import is_python_build
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 from .serializers import PostSerializer
 from .models import Post
 
-class PublicPostListAPIView(generics.ListCreateAPIView):
-    queryset = Post.objects.all() # filter(is_public=True)
-    serializer_class = PostSerializer
+# generics.ListAPIView
+# generics.CreateAPIView
+
+# generics.ListCreateAPIView
+
+# generics.DestroyAPIView
+# generics.UpdateAPIView
+
+# class PublicPostListAPIView(generics.ListAPIView):
+#     queryset = Post.objects.filter(is_public=True)
+#     serializer_class = PostSerializer
+
+# class PublicPostListAPIView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+
+# public_post_list = PublicPostListAPIView.as_view()
+
+@api_view(['GET'])
+def public_post_list(request):
+    qs = Post.objects.filter(is_public=True)
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data)
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
@@ -27,3 +51,13 @@ class PostViewSet(ModelViewSet):
 # def post_detail(request, pk):
 #     # request.method # => 3개 분기
 #     pass
+
+
+# @csrf_exempt
+# def post_list(request):
+#     pass
+
+# def post_list(request):
+#     pass
+
+# post_list = csrf_exempt(post_list) 
